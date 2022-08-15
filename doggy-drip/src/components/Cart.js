8394cc44ca84a216c8edf90c54618dd4e89dcb5c
Yanik Nayee <yaniknayee123@gmail.com>
2022-08-15 18:26:19 -0400

    Added some functionality to cart page

--- doggy-drip/src/components/Cart.js
+++ doggy-drip/src/components/Cart.js
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { CartState } from "../context/Context";
+import { ListGroup } from "react-bootstrap";
 
 export default function Cart() {
-    const {state: {cart} } = CartState();
+    const {state: {cart}, dispatch, } = CartState();
 
-    console.log(cart)
+    console.log(cart.length)
+
+    const [total, setTotal] = useState();
+
+    useEffect(() => {
+        setTotal(cart.reduce((acc, curr) => acc + Number(curr.price)*(curr.qty), 0));
+    }, [cart]);
 
     return(
-        <h1>0</h1>
-    )
+        <div className="cart-page container mt-5">
+            <div className="product-container">
+                <ListGroup>
+                    {cart.map(prod => (
+                        <span>{prod.name}</span>
+                    ))}
+                </ListGroup>
+            </div>
+            <div className="summary">
+                <h3>Subtotal {cart.length} items </h3>
+                <h3>Total ${total}</h3>
+            </div>
+        </div>
+        );
 }
